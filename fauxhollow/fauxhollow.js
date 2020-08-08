c6de106d086f09a8d5ad59cbf540fb8886331f2d
Bedo9041 <7407004+Bedo9041@users.noreply.github.com>
2020-08-08 02:06:48 +0100

    Added solve for fox, not recommended

--- fauxhollow/fauxhollow.js
+++ fauxhollow/fauxhollow.js
@@ -114,7 +114,93 @@ var FauxHollow = {
                     }
                 }
                 break;
+            case "fox":
+                counts = new Array(36).fill(100);
+                for (var row = 0; row <= 4; row++) {
+                    for (var col = 0; col <= 4; col++) {
+                        var index = col + row * 6;
+                        var chestInArea = 0;
+                        var accept = true;
+                        for (var i = 0; i <= 1; i++) {
+                            for (var j = 0; j <= 1; j++) {
+                                if (board[index + j + i * 6] === "C") {
+                                    chestInArea++;
+                                } else if (board[index + j + i * 6] !== "X") {
+                                    accept = false;
+                                }
+                            }
+                        }
+                        if (accept && chestCount <= chestInArea) {
+                            for (var i = 0; i <= 1; i++) {
+                                for (var j = 0; j <= 1; j++) {
+                                    if (board[index + j + i * 6] !== "C") {
+                                        counts[index + j + i * 6] -= 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                for (var row = 0; row <= 3; row++) { // FIRST PASS, LOOKING FOR 3 HIGH, 2 WIDE
+                    for (var col = 0; col <= 4; col++) {
+                        var index = col + row * 6;
+                        var swordInArea = 0;
+                        var accept = true;
+                        for (var i = 0; i <= 2; i++) {
+                            for (var j = 0; j <= 1; j++) {
+                                if (board[index + j + i * 6] === "S") {
+                                    swordInArea++;
+                                } else if (board[index + j + i * 6] !== "X") {
+                                    accept = false;
+                                }
+                            }
+                        }
+                        if (accept && swordCount <= swordInArea) {
+                            for (var i = 0; i <= 2; i++) {
+                                for (var j = 0; j <= 1; j++) {
+                                    if (board[index + j + i * 6] !== "S") {
+                                        counts[index + j + i * 6] -= 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                for (var row = 0; row <= 4; row++) { // SECOND PASS, LOOKING FOR 2 HIGH, 3 WIDE
+                    for (var col = 0; col <= 3; col++) {
+                        var index = col + row * 6;
+                        var swordInArea = 0;
+                        var accept = true;
+                        for (var i = 0; i <= 1; i++) {
+                            for (var j = 0; j <= 2; j++) {
+                                if (board[index + j + i * 6] === "S") {
+                                    swordInArea++;
+                                } else if (board[index + j + i * 6] !== "X") {
+                                    accept = false;
+                                }
+                            }
+                        }
+                        if (accept && swordCount <= swordInArea) {
+                            for (var i = 0; i <= 1; i++) {
+                                for (var j = 0; j <= 2; j++) {
+                                    if (board[index + j + i * 6] !== "S") {
+                                        counts[index + j + i * 6] -= 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                for (var row = 0; row <= 5; row++) { // SECOND PASS, LOOKING FOR 2 HIGH, 3 WIDE
+                    for (var col = 0; col <= 5; col++) {
+                        if (board[col + row * 6] === "L") {
+                            counts[col + row * 6] = 0;
+                        }
+                    }
+                }
+                break;
             case "both":
+            default:
                 for (var row = 0; row <= 4; row++) {
                     for (var col = 0; col <= 4; col++) {
                         var index = col + row * 6;
